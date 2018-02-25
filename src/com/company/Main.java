310d955b1d6c193b6b9d350bda4232cf366890ff
Mighn <bartosz.gala@mail.ru>
2018-02-25 23:22:50 +0100

    First
    It is first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        int a = 1;
+        for (a=0; a<10; a++) {
+            System.out.println(a);
+            a++;
+        }
 	// write your code here
     }
 }
