3ac2515704fd96fe0b7d66b59d9730a347f9a8aa
Mighn <bartosz.gala@mail.ru>
2018-02-25 23:38:43 +0100

    Ok, look good..
    Possible is git works good... at now ;)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,9 @@
 
 package com.company;
 
+import java.awt.*;
+import java.util.concurrent.SynchronousQueue;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,6 +17,7 @@ public class Main {
             a++;
                     }
         System.out.println("Yeah Good :) ");
+        System.out.println("Java at now is really close to C++ :P ");
 	// write your code here
     }
 }
