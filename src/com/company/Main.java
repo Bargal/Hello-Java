7e5224b99292e83408442348b02816903abc01f8
Mighn <bartosz.gala@mail.ru>
2018-02-25 23:06:13 +0100

    Uh, I use GitBash now... (normal I use Visual Studio GIT addon :P ) It is my first time in Java and maybe third try to use Git from bash :D

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+/*
+* It is My first java experience :)
+*/
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+	// write your code here
+    }
+}
