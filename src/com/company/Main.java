77d5df5032c3549c46e51f2cc258b01ab2036e1e
Mighn <bartosz.gala@mail.ru>
2018-02-27 14:19:28 +0100

    arrays
    First little diferent betwen Java and C++ ;)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,6 +21,65 @@ public class Main {
         System.out.println(a);
         System.out.println("I'm interested in about 'a' in previous line...");
 
+        int x = 5, y = 43;
+
+                if (x == y)
+                    System.out.println("x = y");
+                else if (x < y)
+                    System.out.println("x < y");
+                else
+                    System.out.println("x > y");
+
+
+                int sw = 101;
+
+                switch (sw)
+                {
+                    case 10:
+                        System.out.println("sw = 10");
+                        break;
+
+                    case 20:
+                        System.out.println("sw = 20");
+                        break;
+
+                    default:
+                        System.out.println("sw is equal " + sw +" :)");
+                        break;
+
+                }
+
+              // int tab[10]; - not working... c++ dev cry :P
+
+                int[] tab;
+                tab = new int[10];
+
+                //hmm... look like C++ pointer :D
+        int[] tab1 = new int[10];
+
+        int[] tab2 = {1,2,3,6,4,3};
+
+        System.out.println("tab2 length is " + tab2.length);
+
+
+        int [][] tab2d = new int [4][4];
+
+        int [][] tab2d1 =
+                {
+                        {1,2,3,4},
+                        {32,23,21,43},
+                        {0,0,0,0},
+                        {45,32,89,55}
+                };
+
+
+
+
+
+
+
+
+
 
     }
 }
